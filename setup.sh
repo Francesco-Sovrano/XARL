d2abdaf095b9539514c4b188f9f54fab7a20ffb5
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2021-10-16 18:20:13 +0200

    1) Added README.
    1) Added README.

--- setup.sh
+++ setup.sh
@@ -1,3 +1,15 @@
+MY_DIR="`python -c "import os; print(os.path.realpath('$1'))"`"
+cd $MY_DIR
+
+
+# if [ ! -d ".env" ]; then
+# 	sh $MY_DIR/setup_python_env.sh 3.8 6 $MY_DIR .env
+# fi
+# . .env/bin/activate	
+
+virtualenv .env -p python3
+source .env/bin/activate
+
 # echo 'Create a virtual environment'
 # python3 -m venv .env
 # echo 'Activate the virtual environment'
