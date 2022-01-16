9e0564676e74edd99b808f5f3b5ce73e9bd7f7d8
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2022-01-16 11:19:54 +0100

    1) Removed XADQN and XASAC from Shepherd demos, because incompatible with the env or multi-agency. 2) Added XADDPG demo.
    1) Removed XADQN and XASAC from Shepherd demos, because incompatible with the env or multi-agency.
    2) Added XADDPG demo.

--- demos/marl/shepherd/demo_xaddpg_ma.py
+++ demos/marl/shepherd/demo_xaddpg_ma.py
@@ -8,7 +8,7 @@ import ray
 import time
 from xarl.utils.workflow import train
 
-from xarl.agents.xadqn import XADQNTrainer, XADQN_DEFAULT_CONFIG
+from xarl.agents.xaddpg import XADDPGTrainer, XADDPG_DEFAULT_CONFIG
 from environments import *
 from xarl.models.dqn import TFAdaptiveMultiHeadDQN
 from ray.rllib.models import ModelCatalog
@@ -24,44 +24,32 @@ SELECT_ENV = "Shepherd"
 CENTRALISED_TRAINING = True
 NUM_AGENTS = 5
 
-CONFIG = XADQN_DEFAULT_CONFIG.copy()
+CONFIG = XADDPG_DEFAULT_CONFIG.copy()
 CONFIG["env_config"] = {
 	'num_dogs': NUM_AGENTS,
 	'num_sheep': 50,
 }
 CONFIG.update({
 	"horizon": 2**10, # Number of steps after which the episode is forced to terminate. Defaults to `env.spec.max_episode_steps` (if present) for Gym envs.
-	"model": { # this is for GraphDrive and GridDrive
-		"custom_model": "adaptive_multihead_network",
-	},
+	"centralised_buffer": True, # for MARL
+
 	# "preprocessor_pref": "rllib", # this prevents reward clipping on Atari and other weird issues when running from checkpoints
+	"gamma": 0.999, # We use an higher gamma to extend the MDP's horizon; optimal agency on GraphDrive requires a longer horizon.
 	"seed": 42, # This makes experiments reproducible.
-	"rollout_fragment_length": 2**6, # Divide episodes into fragments of this many steps each during rollouts. Default is 1.
+	"rollout_fragment_length": 1, # Divide episodes into fragments of this many steps each during rollouts. Default is 1.
 	"train_batch_size": 2**8, # Number of transitions per train-batch. Default is: 100 for TD3, 256 for SAC and DDPG, 32 for DQN, 500 for APPO.
 	# "batch_mode": "truncate_episodes", # For some clustering schemes (e.g. extrinsic_reward, moving_best_extrinsic_reward, etc..) it has to be equal to 'complete_episodes', otherwise it can also be 'truncate_episodes'.
 	###########################
 	"prioritized_replay": True, # Whether to replay batches with the highest priority/importance/relevance for the agent.
 	'buffer_size': 2**14, # Size of the experience buffer. Default 50000
-	"learning_starts": 2**14, # How many steps of the model to sample before learning starts.
 	"prioritized_replay_alpha": 0.6,
-	"prioritized_replay_beta": 0.4, # The smaller this is, the stronger is over-sampling
+	"prioritized_replay_beta": 0.4, # The smaller, the stronger is over-sampling
 	"prioritized_replay_eps": 1e-6,
+	"learning_starts": 2**14, # How many steps of the model to sample before learning starts.
 	###########################
-	# 'lr': .0000625,
-	# 'adam_epsilon': .00015,
-	# 'exploration_config': {
-	# 	'epsilon_timesteps': 200000,
-	# 	'final_epsilon': 0.01,
-	# },
-	# 'timesteps_per_iteration': 10000,
-	###########################
-	"grad_clip": None, # no need of gradient clipping with huber loss
-	"dueling": True,
-	"double_q": True,
-	"num_atoms": 21,
-	# "v_max": 2**5,
-	# "v_min": -1,
-	"clip_rewards": False,
+	# "tau": 1e-4, # The smaller tau, the lower the value over-estimation, the higher the bias
+	# "grad_clip": 40, # This prevents giant gradients and so improves robustness
+	# "l2_reg": 1e-6, # This mitigates over-fitting
 	##################################
 	"buffer_options": {
 		'priority_id': 'td_errors', # Which batch column to use for prioritisation. Default is inherited by DQN and it is 'td_errors'. One of the following: rewards, prev_rewards, td_errors.
@@ -69,7 +57,6 @@ CONFIG.update({
 		'priority_aggregation_fn': 'np.mean', # A reduction that takes as input a list of numbers and returns a number representing a batch priority.
 		'cluster_size': None, # Default None, implying being equal to global_size. Maximum number of batches stored in a cluster (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'replay_sequence_length' (default is 1).
 		'global_size': 2**14, # Default 50000. Maximum number of batches stored in all clusters (which number depends on the clustering scheme) of the experience buffer. Every batch has size 'replay_sequence_length' (default is 1).
-		'clustering_xi': 1, # Let X be the minimum cluster's size, and q be the clustering_xi, then the cluster's size is guaranteed to be in [X, X+qX]. This shall help having a buffer reflecting the real distribution of tasks (where each task is associated to a cluster), thus avoiding over-estimation of task's priority.
 		'prioritization_alpha': 0.6, # How much prioritization is used (0 - no prioritization, 1 - full prioritization).
 		'prioritization_importance_beta': 0.4, # To what degree to use importance weights (0 - no corrections, 1 - full correction).
 		'prioritization_importance_eta': 1e-2, # Used only if priority_lower_limit is None. A value > 0 that enables eta-weighting, thus allowing for importance weighting with priorities lower than 0 if beta is > 0. Eta is used to avoid importance weights equal to 0 when the sampled batch is the one with the highest priority. The closer eta is to 0, the closer to 0 would be the importance weight of the highest-priority batch.
@@ -79,6 +66,7 @@ CONFIG.update({
 		'cluster_prioritisation_strategy': 'sum', # Whether to select which cluster to replay in a prioritised fashion -- Options: None; 'sum', 'avg', 'weighted_avg'.
 		'cluster_prioritization_alpha': 1, # How much prioritization is used (0 - no prioritization, 1 - full prioritization).
 		'cluster_level_weighting': True, # Whether to use only cluster-level information to compute importance weights rather than the whole buffer.
+		'clustering_xi': 1, # Let X be the minimum cluster's size, and C be the number of clusters, and q be clustering_xi, then the cluster's size is guaranteed to be in [X, X+(q-1)CX], with q >= 1, when all clusters have reached the minimum capacity X. This shall help having a buffer reflecting the real distribution of tasks (where each task is associated to a cluster), thus avoiding over-estimation of task's priority.
 		# 'clip_cluster_priority_by_max_capacity': False, # Whether to clip the clusters priority so that the 'cluster_prioritisation_strategy' will not consider more elements than the maximum cluster capacity.
 		'max_age_window': None, # Consider only batches with a relative age within this age window, the younger is a batch the higher will be its importance. Set to None for no age weighting. # Idea from: Fedus, William, et al. "Revisiting fundamentals of experience replay." International Conference on Machine Learning. PMLR, 2020.
 	},
@@ -86,14 +74,13 @@ CONFIG.update({
 	"clustering_scheme_options": {
 		"episode_window_size": 2**6, 
 		"batch_window_size": 2**8, 
-		"n_clusters": 4,
+		"n_clusters": 8,
 	},
 	"cluster_selection_policy": "min", # Which policy to follow when clustering_scheme is not "none" and multiple explanatory labels are associated to a batch. One of the following: 'random_uniform_after_filling', 'random_uniform', 'random_max', 'max', 'min', 'none'
 	"cluster_with_episode_type": False, # Useful with sparse-reward environments. Whether to cluster experience using information at episode-level.
 	"cluster_overview_size": 1, # cluster_overview_size <= train_batch_size. If None, then cluster_overview_size is automatically set to train_batch_size. -- When building a single train batch, do not sample a new cluster before x batches are sampled from it. The closer cluster_overview_size is to train_batch_size, the faster is the batch sampling procedure.
 	"collect_cluster_metrics": False, # Whether to collect metrics about the experience clusters. It consumes more resources.
 	"ratio_of_samples_from_unclustered_buffer": 0, # 0 for no, 1 for full. Whether to sample in a randomised fashion from both a non-prioritised buffer of most recent elements and the XA prioritised buffer.
-	"centralised_buffer": True, # for MARL
 })
 CONFIG["callbacks"] = CustomEnvironmentCallbacks
 
@@ -133,4 +120,4 @@ CONFIG.update({
 ray.shutdown()
 ray.init(ignore_reinit_error=True, include_dashboard=False)
 
-train(XADQNTrainer, CONFIG, SELECT_ENV, test_every_n_step=1e7, stop_training_after_n_step=2e7)
+train(XADDPGTrainer, CONFIG, SELECT_ENV, test_every_n_step=4e7, stop_training_after_n_step=4e7)
