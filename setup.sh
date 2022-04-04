b4183a30dc8862d508e51f5a22482aeb76721660
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2022-04-04 14:58:47 +0100

    1) Removed single-agent environments and related stuff.
    1) Removed single-agent environments and related stuff.

--- setup.sh
+++ setup.sh
@@ -23,16 +23,3 @@ pip install -r environments/requirements.txt
 
 echo 'Installing XARL..'
 pip install -e ./package # cmake is needed
-
-echo 'Installing Atari ROMs..'
-pip install gym[atari]==0.14.0
-mkdir Atari-ROM
-cd Atari-ROM
-wget http://www.atarimania.com/roms/Roms.rar
-unrar x Roms.rar
-unzip ROMS.zip
-python -m atari_py.import_roms ROMS
-cd ..
-
-echo 'Fixing environments rendering'
-pip install pyglet==1.5.11 # fix for rendering environments
