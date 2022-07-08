1366a7adb3e8507fdc6e475c8d02473ad7baef00
francesco.sovrano <38495561+Francesco-Sovrano@users.noreply.github.com>
2022-07-08 14:51:45 +0100

    1) File system refactor: renaming car_controller into bot_controller
    1) File system refactor: renaming car_controller into bot_controller

--- environments/__init__.py
+++ environments/__init__.py
@@ -1,6 +1,6 @@
 from .custom_metrics import CustomEnvironmentCallbacks
 
-from .car_controller import *
+from .bot_controller import *
 # from .special_atari import *
 # from .shepherd import *
 # from .gfootball import *
